6aa0213ebab22d6dcfb11d1db729289e6767f8f5
antonymaciel <antonymaciel.dx@gmail.com>
2024-08-06 13:38:06 -0300

    add basic timer queue example

---
+++ timer_queue/timer_queue_basic.js
@@ -0,0 +1,63 @@
+// index.js
+setTimeout(() => console.log("setTimeout 1"), 1000);
+setTimeout(() => console.log("setTimeout 2"), 500);
+setTimeout(() => console.log("setTimeout 3"), 0);
+
+for (let i = 0; i <= 1000000000; i++) { if (i === 1000000000) {console.log('forLoop');} } // to avoid ambiguity with the timers
+
+/*
+----------------------Loop 1 - Start ----------------------
+
+// Output: forLoop
+// setTimeout 3, setTimeout 2 and setTiemout 1 finished 
+
+STATUS: 
+- Microtask nextTick queue: []
+- Microtask Others queue: []
+- Timers queue: [setTimeout 3, setTimeout 2, setTiemout 1]
+- I/O queue: []
+- Check Immediate queue: []
+- Close queue: []
+
+----------------------Loop 1 - Timer phase: start ----------------------
+
+// Output: setTimeout 3
+
+STATUS: 
+- Microtask nextTick queue: []
+- Microtask Others queue: []
+- Timers queue: [setTimeout 2, setTiemout 1]
+- I/O queue: []
+- Check Immediate queue: []
+- Close queue: []
+
+// Output: setTimeout 2
+
+STATUS: 
+- Microtask nextTick queue: []
+- Microtask Others queue: []
+- Timers queue: [setTiemout 1]
+- I/O queue: []
+- Check Immediate queue: []
+- Close queue: []
+
+// Output: setTimeout 1
+
+STATUS: 
+- Microtask nextTick queue: []
+- Microtask Others queue: []
+- Timers queue: []
+- I/O queue: []
+- Check Immediate queue: []
+- Close queue: []
+
+---------------------- The End ----------------------
+*/
+
+// Output summary
+/*
+forLoop
+setTimeout 3
+setTimeout 2
+setTimeout 1
+*/
