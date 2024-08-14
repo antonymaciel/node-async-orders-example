1f0ad7c413f56338960f4ace7f720582753b47d5
antonymaciel <antonymaciel.dx@gmail.com>
2024-08-14 13:05:00 -0300

    io queue variation example

---
+++ io_queue/io_and_timer_and_microtask_queue.js
@@ -0,0 +1,93 @@
+const fs = require("fs");
+
+fs.readFile(__filename, () => {
+  console.log("readFile");
+});
+
+process.nextTick(() => console.log("nextTick"));
+Promise.resolve().then(() => console.log("Promise"));
+setTimeout(() => console.log("setTimeout"), 0);
+
+for (let i = 0; i <= 1000000000; i++) { if (i === 1000000000) {console.log('forLoop');} } // to avoid ambiguity
+
+
+/*
+----------------------Loop 1 - Start ----------------------
+
+// I/O readFile starts
+// Timer setTimeout starts
+// microstasks enqueued
+// Output: forLoop
+// Timer setTimeout and I/O readFile finished
+
+STATUS: 
+- Microtask nextTick queue: [nextTick]
+- Microtask Others queue: [Promise]
+- Timers queue: [setTimeout]
+- I/O queue: [readFile]
+- Check Immediate queue: []
+- Close queue: []
+
+----------------------Loop 1 - Timer phase: start ----------------------
+----------------------Loop 1 - Timer phase / Microtasks: Next ticks ----------------------
+
+// Output: nextTick
+
+STATUS: 
+- Microtask nextTick queue: []
+- Microtask Others queue: [Promise]
+- Timers queue: [setTimeout]
+- I/O queue: [readFile]
+- Check Immediate queue: []
+- Close queue: []
+
+----------------------Loop 1 - Timer phase / Microtasks: Others ----------------------
+
+// Output: Promise
+
+STATUS: 
+- Microtask nextTick queue: []
+- Microtask Others queue: []
+- Timers queue: [setTimeout]
+- I/O queue: [readFile]
+- Check Immediate queue: []
+- Close queue: []
+
+----------------------Loop 1 - Timer phase: resume ----------------------
+
+// Output: setTimeout
+
+STATUS: 
+- Microtask nextTick queue: []
+- Microtask Others queue: []
+- Timers queue: []
+- I/O queue: [readFile]
+- Check Immediate queue: []
+- Close queue: []
+
+----------------------Loop 1 - I/O phase ----------------------
+
+// Output: readFile
+
+STATUS: 
+- Microtask nextTick queue: []
+- Microtask Others queue: []
+- Timers queue: []
+- I/O queue: []
+- Check Immediate queue: []
+- Close queue: []
+
+---------------------- The End ----------------------
+*/
+
+
+
+
+// Output summary
+/*
+forLoop
+nextTick
+Promise
+setTimeout
+readFile
+*/
